2a51f6db512116461431e716962f31cb99c5bf3d
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2016-02-29 14:09:48 +0100

    Update setupdata.sh

--- setupdata.sh
+++ setupdata.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-
+echo "web.addons.1.name=Shellinabox" >> /etc/rpimonitor/data.conf
+echo "web.addons.1.addons=shellinabox" >> /etc/rpimonitor/data.conf
+echo "include=/etc/rpimonitor/template/storage.conf" >> /etc/rpimonitor/data.conf
+echo "include=/etc/rpimonitor/template/services.conf" >> /etc/rpimonitor/data.conf
+echo "include=/etc/rpimonitor/template/wlan.conf" >> /etc/rpimonitor/data.conf
+echo "include=/etc/rpimonitor/template/dht11.conf" >> /etc/rpimonitor/data.conf
